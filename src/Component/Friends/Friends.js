a36279a1e9b37ed7207c84ea66020c1701154db7
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-03 16:59:47 +0600

    new commit

---
+++ src/Component/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>here is friend component</h1>
+        </div>
+    );
+};
+
+export default Friends;
