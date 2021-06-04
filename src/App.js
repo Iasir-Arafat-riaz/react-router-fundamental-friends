c70ea432f8e18c1dded01dd2e355aecd8588cf56
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-05 04:30:20 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,28 @@ import {
   Link
 } from "react-router-dom";
 import Home from './Component/Home/Home';
+import NoMatch from "./Component/NoMatch/NoMatch"
 
 
 function App() {
-  
+
   return (
     <div className="App">
-      <Home/>
-      
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home/>
+
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
