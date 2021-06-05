dda9d8d5a32a485a24c7124cbd17635f6ff09478
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-05 06:40:30 +0600

    new commit

--- src/Component/NoMatch/NoMatch.js
+++ src/Component/NoMatch/NoMatch.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NoMatch = () => {
     return (
         <div>
-            <h2> This Page Not Found</h2>
+            <h2> This Page Not Found 404</h2>
+            <h1>back to home page</h1>
         </div>
     );
 };
