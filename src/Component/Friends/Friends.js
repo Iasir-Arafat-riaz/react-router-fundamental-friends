16aba1cb2a6a7915700467ac22468d2f8d730ab3
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-06 19:46:56 +0600

    id add

--- src/Component/Friends/Friends.js
+++ src/Component/Friends/Friends.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Friends = (props) => {
-    const{name,email}=props.friend
+    const{name,email,id}=props.friend
     const friendStyle = {border:"2px solid blue", margin:"20px", borderRadius:"10px"}
 
     return (
         <div style={friendStyle}>
-            <h1>{name}</h1>
+            <h1>{id} {name}</h1>
             <p>email : {email}</p>
         </div>
     );
