6f592a77c1f9efa38efcf69b4a190cbcce322ce3
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-07 04:20:07 +0600

    new commit

--- src/Component/Friends/Friends.js
+++ src/Component/Friends/Friends.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Friends = (props) => {
     const{name,email,id}=props.friend
+    const xd = props.friend.company.bs
     const friendStyle = {border:"2px solid blue", margin:"20px", borderRadius:"10px"}
 
     return (
         <div style={friendStyle}>
-            <h1>{id} {name}</h1>
+            <h1>{id}  {name}</h1>
             <p>email : {email}</p>
+            <h4>{xd}</h4>
         </div>
     );
 };
