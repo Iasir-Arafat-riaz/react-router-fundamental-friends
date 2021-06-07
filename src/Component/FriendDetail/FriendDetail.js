dd5dc1a19b617c70a05d33379a849bf05216084d
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-07 06:16:17 +0600

    new commit

--- src/Component/FriendDetail/FriendDetail.js
+++ src/Component/FriendDetail/FriendDetail.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const FriendDetail = () => {
+    const{friendId}=useParams()
     return (
         <div>
-            <h1>This is friend details</h1>
+            <h1>This is friend details {friendId}</h1>
             <h2>give us your detail</h2>
         </div>
     );
