734f190765ce7020b7b7a2863270cb0d264d5dc3
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-10 05:33:28 +0600

    new commit

--- src/Component/FriendDetail/FriendDetail.js
+++ src/Component/FriendDetail/FriendDetail.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const FriendDetail = () => {
     const{friendId}=useParams()
+    const [friendDetail,setFriendDetail]=useState({})
+    useEffect(()=>{
+      fetch(`https://jsonplaceholder.typicode.com/users/${friendId}`)
+    },[])
     return (
         <div>
             <h1>This is friend details {friendId}</h1>
