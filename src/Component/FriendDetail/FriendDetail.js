5478b56a25647434068b489956784c079f149591
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-06-10 05:40:50 +0600

    new commit

--- src/Component/FriendDetail/FriendDetail.js
+++ src/Component/FriendDetail/FriendDetail.js
@@ -6,11 +6,15 @@ const FriendDetail = () => {
     const [friendDetail,setFriendDetail]=useState({})
     useEffect(()=>{
       fetch(`https://jsonplaceholder.typicode.com/users/${friendId}`)
-    },[])
+      .then(res=>res.json())
+      .then(data=>setFriendDetail(data))
+    },[]);
+
     return (
         <div>
             <h1>This is friend details {friendId}</h1>
             <h2>give us your detail</h2>
+            <h3>here{friendDetail.name}</h3>
         </div>
     );
 };
